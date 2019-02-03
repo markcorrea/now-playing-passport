df0e379bd18a2c0c1355853c81e49af125e3dc66
Marcus Correa <marcus.correa@taller.net.br>
2019-02-03 16:00:45 -0200

    Added Credential on header settings.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json())
 /* Defining CORS */
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
+  res.header("Access-Control-Allow-Credentials", true);
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
